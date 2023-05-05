b27601357fd7b2695016d24602e9400e35a059da
AminataAlidjiMaiga <aminatamaiga69@yahoo.com>
2023-05-05 19:58:16 +0200

    refresh

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -5,45 +5,70 @@ public class Welcome {
 	public static String affichage(String nom) {
 		StringBuilder retour = new StringBuilder();
 		StringBuilder maj= new StringBuilder();
-		retour.append("Hello,");
-		maj.append(". AND HELLO,");
+		maj.append(".AND HELLO");
+		retour.append("Hello");
 		if (isempty(nom)) {
+			retour.append(",");
 			retour.append("my friend");
 		}
-		// Suppression des espaces inutiles
-	    nom = nom.trim().replaceAll("\\s+", " ");
-		String[] nameList = nom.split(",");
-		if (nameList.length==1) {
-			if (nameList[0].equals(nameList[0].toUpperCase())) {
-				retour.toString().toUpperCase();
-				retour.append(nom);
-			}
-			retour.append(nameList[0]);
-		}
 		else {
-			for (int i=0;i<(nameList.length)-1;i++) {
-				if (nameList[i].equals(nameList[i].toUpperCase()))
-				{
-					maj.append(nameList[i]);
+			// Suppression des espaces inutiles
+		    nom = nom.trim().replaceAll("\\s+", " ");
+			String[] nameList = nom.split(",");
+			if (nameList.length==1) {
+				if (nameList[0].equals(nameList[0].toUpperCase())) {
+					retour.replace(0,5,"HELLO");
+					retour.append(",");
+					retour.append(nom);
+					retour.append(" !");
 				}
 				else {
-					retour.append(nameList[i]);
-				}	
+					retour.append(",");
+					retour.append(trans(nameList[0]));
+				}
 			}
-			maj.append("!");
-			retour.append("and");
-			retour.append(nameList[-1]);
-			retour.append(maj);
+			else if (nameList.length==2) {
+				for (int i=0;i<(nameList.length);i++) {
+					retour.append(",");
+					retour.append(trans(nameList[i]));
+				}
+			}
+			else {
+				boolean ishere=false;
+				for (int i=0;i<(nameList.length);i++) {
+					if (nameList[i].equals(nameList[i].toUpperCase()))
+					{
+						retour.append(",");
+						maj.append(nameList[i]);
+						ishere=true;
+					}
+					else {
+						retour.append(",");
+						retour.append(trans(nameList[i]));
+					}	
+				}
+				if (ishere) {
+					maj.append("!");
+					retour.append(maj);
+				}
+				//retour.append("and");
+				//retour.append(nameList[(nameList.length)-1]);
+			}
+			
 		}
 		return retour.toString();
-
 	}
 	
 	public static boolean isempty(String input) {
 		boolean res=false;
-		if (input.equals(" ") || input==null) {
+		if (input.equals(" ") || input.equals("  ")) {
 			res=true;
 		}
 		return res;
 	}
+	
+	public static String trans(String input) {
+		return input.substring(0,1).toUpperCase()+input.subSequence(1,input.length());
+	}
+	
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -17,38 +17,45 @@ class WelcomeTest {
 	
 	@Test
 	void test1() {
-		assertEquals(Welcome.affichage("bob"),"Hello,bob");
-	}
-	
-	void test2() {
-		assertEquals(Welcome.affichage(" "),"Hello, my friend");
 		assertEquals(Welcome.affichage("bob"),"Hello,Bob");
-		assertEquals(Welcome.affichage("  "),"Hello, my friend");
 	}
 	
+	@Test
+	void test2() {
+		assertEquals(Welcome.affichage(" "),"Hello,my friend");
+		assertEquals(Welcome.affichage("  "),"Hello,my friend");
+	}
+	@Test
 	void test3() {
-		assertEquals(Welcome.affichage("JERRY"),"HELLO, JERRY");
+		assertEquals(Welcome.affichage("JERRY"),"HELLO,JERRY !");
 	}
 	
+	@Test
 	void test4() {
-		assertEquals(Welcome.affichage("Amy,Bob"),"Hello, Amy, Bob”");
-		
+		assertEquals(Welcome.affichage("Amy,Bob"),"Hello,Amy,Bob");
+		assertEquals(Welcome.affichage("aminata,dina"),"Hello,Aminata,Dina");
 	}
+	
+	@Test
 	void test5() {
-		assertEquals(Welcome.affichage("Amy,Bob,Jerry"),"Hello, Amy, Bob, Jerry”");
-		assertEquals(Welcome.affichage("Amy,Bob,Jerry,Tom,Bambi"),"Hello, Amy, Bob, Jerry, Tom, Bambi");
+		assertEquals(Welcome.affichage("Amy,Bob,Jerry"),"Hello,Amy,Bob,Jerry");
+		assertEquals(Welcome.affichage("Amy,Bob,Jerry,Tom,Bambi"),"Hello,Amy,Bob,Jerry,Tom,Bambi");
 	}
+	
+	@Test
 	void test6() {
-		assertEquals(Welcome.affichage("Amy, BOB, Jerry"),"Hello, Amy, Jerry. AND HELLO, BOB !");
-		assertEquals(Welcome.affichage("Amy, BOB, TOM, Jerry"),"Hello, Amy, Jerry. AND HELLO, BOB, TOM !");
+		assertEquals(Welcome.affichage("Amy, BOB, Jerry"),"Hello, Amy, Jerry. AND HELLO, BOB!");
 	}
+	
 	void test7() {
-		assertEquals(Welcome.affichage("Bob, Amy, Jerry"),"Hello, Amy and Jerry!");
+		assertEquals(Welcome.affichage("Bob, Amy, Jerry"),"Hello, Amy and Jerry");
 		assertEquals(Welcome.affichage("Bob, AMY, Jerry, JACK"),"Hello, bob and jerry AND HELLO AMY AND JACK !");
 		assertEquals(Welcome.affichage("Bob, AMY, Jerry, JACK, TOM, Aminata"),"Hello, bob,jerry and Aminata. AND HELLO AMY AND JACK AND TOM !");
+		assertEquals(Welcome.affichage("Amy, BOB, TOM, Jerry"),"Hello, Amy, Jerry. AND HELLO, BOB, TOM !");
 	}
+	
 	void test8() {
-		assertEquals(Welcome.affichage("Bob , Amy    "),"“Hello, Bob and Amy");
+		assertEquals(Welcome.affichage("bob , Amy    "),"“Hello, Bob and Amy");
 		assertEquals(Welcome.affichage("Bob, AMY,         Jerry,       JACK, TOM,       Aminata"),"Hello, bob,jerry and Aminata. AND HELLO AMY AND JACK AND TOM !");
 
 	}
